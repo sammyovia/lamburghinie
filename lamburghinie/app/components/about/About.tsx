af397e6486379a08c5a367f7bb1f83a93d14caa8
Sammyovia <107253576+sammyovia@users.noreply.github.com>
2024-02-20 21:02:37 +0100

    hotfix

--- lamburghinie/app/components/about/About.tsx
+++ lamburghinie/app/components/about/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     <div className="flex flex-col items-center justify-center mt-8">
       <h2 className="text-3xl font-bold mb-4">About Us</h2>
       <div className="flex flex-col items-center justify-center">
-        <img src="https://via.placeholder.com/400" alt="About Us" className="rounded-lg mb-4" />
+        <img src="https://img.freepik.com/free-photo/3d-rendering-loft-scandinavian-living-room-with-working-table-bookshelf_105762-2141.jpg?w=1480&t=st=1708459257~exp=1708459857~hmac=37865ca75b29af243de86297bffd549c55b0543c9eaf0e5c69c8c8091f36521e" alt="About Us" className="rounded-lg mb-4" />
         <p className="text-lg text-gray-600">
           Lamburghninie is a Hospitality service company that offers unique services to clients from accomodations, workspaces, short stays, luxurious ride sharing and Event centers. Our accomodations comes with exquisite meals. All of our services can be accessed at your finger tips, or at your voice command.
         </p>
--- lamburghinie/app/components/hero/HeroSection.tsx
+++ lamburghinie/app/components/hero/HeroSection.tsx
@@ -8,7 +8,6 @@ const HeroSection: React.FC<{ image: string; buttonText: string }> = ({ image, b
         <p className="text-lg text-gray-600 mb-8">Hospitality redefined. Book workspaces, accomodations, ride sharing and Event centers. Experience best in class AI powered hospitality services tailored to your taste, designed for your convienience.</p>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Book now</button>
       </div>
-      <img src="https://img.freepik.com/free-photo/3d-rendering-luxury-modern-living-room-with-leather-sofa-lamp_105762-2255.jpg?w=1480&t=st=1708456457~exp=1708457057~hmac=a3a30e1b1952e70815e9904588257bb084e2fb111968064900d340d7d93aed86" alt="Hero Image" className="w-1/2 rounded-lg shadow-lg" />
     </div>
   );
 };
